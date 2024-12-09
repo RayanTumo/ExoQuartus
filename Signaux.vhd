dc9bebb461b9d01b99e15f2a35027933a59606e6
RayanTumo <156229552+RayanTumo@users.noreply.github.com>
2024-12-09 16:02:38 +0100

    Update Signaux.vhd

--- Signaux.vhd
+++ Signaux.vhd
@@ -3,7 +3,7 @@ USE ieee.std_logic_1164.ALL;
 
 ENTITY Signaux IS
 PORT (
-    nb : IN  STD_LOGIC_VECTOR(3 DOWNTO 0); -- Entrée : le nombre binaire
+    nb : IN  STD_LOGIC_VECTOR(3 DOWNTO 0); -- EntrÃ©e : le nombre binaire
     a : OUT STD_LOGIC;
     b : OUT STD_LOGIC;
     c : OUT STD_LOGIC;
@@ -21,18 +21,19 @@ BEGIN
 -- Mapping des segments pour chaque chiffre
 WITH nb SELECT
 AfficheCompteur <=
-    "0111111" WHEN "0001", -- 1
-    "1011011" WHEN "0010", -- 2
-    "1001111" WHEN "0011", -- 3
-    "1100110" WHEN "0100", -- 4
-    "1101101" WHEN "0101", -- 5
-    "1111101" WHEN "0110", -- 6
-    "0000111" WHEN "0111", -- 7
-    "1111111" WHEN "1000", -- 8
-    "1101111" WHEN "1001", -- 9
-    "1001111" WHEN OTHERS; -- E pour les autres valeurs
+    "1000000" WHEN "0000", --0
+    "1111001" WHEN "0001", -- 1
+    "0100100" WHEN "0010", -- 2
+    "0110000" WHEN "0011", -- 3
+    "0011001" WHEN "0100", -- 4
+    "0010010" WHEN "0101", -- 5
+    "0000010" WHEN "0110", -- 6
+    "1111000" WHEN "0111", -- 7
+    "0000000" WHEN "1000", -- 8
+    "0010000" WHEN "1001", -- 9
+    "0000110" WHEN OTHERS; -- E pour les autres valeurs
 
--- Assignation des segments à chaque sortie
+-- Assignation des segments Ã  chaque sortie
 a <= AfficheCompteur(0);
 b <= AfficheCompteur(1);
 c <= AfficheCompteur(2);
